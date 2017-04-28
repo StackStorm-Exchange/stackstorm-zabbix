64b047b91e061b12bd865ff684553087c094b6f3
Daniel Chamot <daniel@nullkarma.com>
2017-04-28 09:36:59 +0200

    pep8 applied

--- stackstorm-foreman/actions/puppet_hosts.py
+++ stackstorm-foreman/actions/puppet_hosts.py
@@ -6,4 +6,7 @@ from lib.foreman import ForemanHosts
 class PuppetHostSearch(ForemanHosts):
 
     def run(self, environment, extended, running, query_filter=None):
-        return True, dict(hosts=self.hosts('environments/{}/hosts'.format(environment), extended, running=running, params=query_filter))
+        return True, dict(hosts=self.hosts('environments/{}/hosts'.format(environment),
+                                           extended,
+                                           running=running,
+                                           params=query_filter))
