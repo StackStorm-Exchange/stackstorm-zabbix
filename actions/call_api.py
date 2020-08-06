d20ca22df9611f5aa445bc11122e663930f8868d
Ian Price <iprice@tableau.com>
2020-08-05 17:04:11 -0700

    Added output and groupids parameters to list_hosts. Added logic to clear unused parameters from being sent through

--- actions/call_api.py
+++ actions/call_api.py
@@ -1,10 +1,22 @@
 from lib.actions import ZabbixBaseAction
 from zabbix.api import ZabbixAPI
 
+import six
+
 
 class CallAPI(ZabbixBaseAction):
     def run(self, api_method, token, **params):
         # Initialize client object to connect Zabbix server
+
+        # Python doesn't let you modify the length of a dict or list while iterating
+        iterate_params = params.copy()
+
+        # Look for Empty strings and None values, but allow False
+        # If None or '', remove it from params.
+        for key, value in six.iteritems(iterate_params):
+            if key in params and not params[key] and not params[key] is False:
+                del params[key]
+
         if token:
             self.client = ZabbixAPI(url=self.config['zabbix']['url'])
             self.auth = token
